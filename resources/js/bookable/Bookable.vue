5a08c99b09829e2043a02a83f04fcbd47bcee29a
sai aung naing tun <saisaiant@gmial.com>
2020-02-15 17:27:07 +0630

    add review list component

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -11,6 +11,7 @@
                     <div v-else>Loading . . .</div>
                 </div>
             </div>
+            <review-list></review-list>
         </div>
         <div class="col-md-4 pb-4">
             <availability />
@@ -20,10 +21,12 @@
 
 <script>
 import Availability from './Availability';
+import ReviewList from './ReviewList';
 
 export default {
     components: {
-        Availability
+        Availability,
+        ReviewList
     },
     data() {
         return  {
---
+++ resources/js/bookable/ReviewList.vue
@@ -0,0 +1,25 @@
+<template>
+    <div style="padding:1.25rem">
+        <h6 class="text-uppercase text-secondary font-weight-bolder pt-4">Review List</h6>
+        <div class="border-bottom" v-for="number in 3" :key="number">
+            <div class="row pt-3">
+                <div class="col-md-6">
+                    Mg mg
+                </div>
+                <div class="col-md-6 d-flex justify-content-end">
+                    Star rating
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    Add 5 min ago
+                </div>
+            </div>
+            <div class="row pt-4 pb-4">
+                <div class="col-12">
+                    Content of the review
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
